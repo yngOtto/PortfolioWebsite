dc6279b4a96b9594dc3c91838ef291747249d26a
Otto <olarsen22@gmail.com>
2023-08-06 00:39:48 +0200

    add form.js script

---
+++ assets/js/form.js
@@ -0,0 +1,24 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  // Get all input elements
+  var inputs = this.getElementsByTagName('input');
+  var textarea = this.getElementsByTagName('textarea');
+
+  // Loop through inputs
+  for (var i = 0; i < inputs.length; i++) {
+    // If an input is empty
+    if (inputs[i].value === '') {
+      // Prevent form submission
+      event.preventDefault();
+      alert('Please fill out all fields before submitting.');
+      return;
+    }
+  }
+
+  // Check if textarea is empty
+  if (textarea[0].value === '') {
+    // Prevent form submission
+    event.preventDefault();
+    alert('Please fill out all fields before submitting.');
+    return;
+  }
+});
