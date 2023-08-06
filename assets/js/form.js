20f79951814c982dc5ef13c36fab308beb6d82c2
Otto <olarsen22@gmail.com>
2023-08-06 10:47:17 +0200

    change form.js to account for new submit button

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,18 +1,14 @@
-document.querySelector('.submit').addEventListener('click', function(event) {
-  // Prevent the link from doing anything
-  event.preventDefault();
-
-  // Get the form element
-  var form = document.getElementById('contactForm');
-
+document.getElementById('contactForm').addEventListener('submit', function(event) {
   // Get all input elements
-  var inputs = form.getElementsByTagName('input');
-  var textarea = form.getElementsByTagName('textarea');
+  var inputs = this.getElementsByTagName('input');
+  var textarea = this.getElementsByTagName('textarea');
 
   // Loop through inputs
   for (var i = 0; i < inputs.length; i++) {
     // If an input is empty
     if (inputs[i].value === '') {
+      // Prevent form submission
+      event.preventDefault();
       alert('Please fill out all fields before submitting.');
       return;
     }
@@ -20,11 +16,10 @@ document.querySelector('.submit').addEventListener('click', function(event) {
 
   // Check if textarea is empty
   if (textarea[0].value === '') {
+    // Prevent form submission
+    event.preventDefault();
     alert('Please fill out all fields before submitting.');
     return;
   }
-
-  // If all fields are filled out, submit the form
-  form.submit();
 });
 
