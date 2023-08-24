65f570488ef0b2134dff2f3c67ff3ace4671e2ca
Otto <olarsen22@gmail.com>
2023-08-24 12:44:23 +0200

    add darkmode toggle to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,23 +5,7 @@
 */
 import Darkmode from 'darkmode-js';
 
-const options = {
-	bottom: '32px', // default: '32px'
-	right: 'unset', // default: '32px'
-	left: '32px', // default: 'unset'
-	time: '0.3s', // default: '0.3s'
-	mixColor: '#fff', // default: '#fff'
-	backgroundColor: '#fff',  // default: '#fff'
-	buttonColorDark: '#100f2c',  // default: '#100f2c'
-	buttonColorLight: '#fff', // default: '#fff'
-	saveInCookies: true, // default: true,
-	label: '🌓', // default: ''
-	autoMatchOsTheme: true // default: true
-  }
-  
-  const darkmode = new Darkmode(options);
-  darkmode.showWidget();
-  
+
 
 (function($) {
 
@@ -207,3 +191,21 @@ const options = {
 			});
 
 })(jQuery);
+
+const options = {
+	bottom: '32px', // default: '32px'
+	right: 'unset', // default: '32px'
+	left: '32px', // default: 'unset'
+	time: '0.3s', // default: '0.3s'
+	mixColor: '#fff', // default: '#fff'
+	backgroundColor: '#fff',  // default: '#fff'
+	buttonColorDark: '#100f2c',  // default: '#100f2c'
+	buttonColorLight: '#fff', // default: '#fff'
+	saveInCookies: true, // default: true,
+	label: '🌓', // default: ''
+	autoMatchOsTheme: true // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+  
