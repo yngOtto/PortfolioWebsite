d8a10e18b0807bac34ffdb53ef0386422b984118
Otto <olarsen22@gmail.com>
2023-08-24 12:45:27 +0200

    remove darkmode toggle to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -192,20 +192,3 @@ import Darkmode from 'darkmode-js';
 
 })(jQuery);
 
-const options = {
-	bottom: '32px', // default: '32px'
-	right: 'unset', // default: '32px'
-	left: '32px', // default: 'unset'
-	time: '0.3s', // default: '0.3s'
-	mixColor: '#fff', // default: '#fff'
-	backgroundColor: '#fff',  // default: '#fff'
-	buttonColorDark: '#100f2c',  // default: '#100f2c'
-	buttonColorLight: '#fff', // default: '#fff'
-	saveInCookies: true, // default: true,
-	label: '🌓', // default: ''
-	autoMatchOsTheme: true // default: true
-  }
-  
-  const darkmode = new Darkmode(options);
-  darkmode.showWidget();
-  
