67a3abc92dd54609a7e55eb3a7efb48068014d9d
Otto <olarsen22@gmail.com>
2023-08-24 12:43:18 +0200

    add darkmode toggle to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,25 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+import Darkmode from 'darkmode-js';
+
+const options = {
+	bottom: '32px', // default: '32px'
+	right: 'unset', // default: '32px'
+	left: '32px', // default: 'unset'
+	time: '0.3s', // default: '0.3s'
+	mixColor: '#fff', // default: '#fff'
+	backgroundColor: '#fff',  // default: '#fff'
+	buttonColorDark: '#100f2c',  // default: '#100f2c'
+	buttonColorLight: '#fff', // default: '#fff'
+	saveInCookies: true, // default: true,
+	label: '🌓', // default: ''
+	autoMatchOsTheme: true // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+  
 
 (function($) {
 
