ae07052675d536544338223e49052a2ea666040c
Otto <olarsen22@gmail.com>
2023-09-13 17:51:20 +0200

    ensure website starts in lightmode

--- assets/js/togglemode.js
+++ assets/js/togglemode.js
@@ -22,5 +22,12 @@ document.getElementById('toggle-dark-mode').addEventListener('click', function()
     icon.classList.remove('fa-moon');
     icon.classList.add('fa-sun');
     icon.style.color = '#f39c12'; // yellow for sun
+  } else if (localStorage.getItem('light-mode') === null) {
+    localStorage.setItem('light-mode', 'enabled');
+    document.body.classList.add('light-mode');
+    var icon = document.getElementById('dark-mode-icon');
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+    icon.style.color = '#f39c12'; // yellow for sun
   }
   
